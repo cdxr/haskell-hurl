8aeb36599e9378f79acca5dbea6d65cb37ee70cb
Craig Roche <cdxr01@gmail.com>
2013-09-20 20:55:24 -0400

    removed extraneous comment

--- Physics/Hurl.hs
+++ Physics/Hurl.hs
@@ -8,11 +8,6 @@ module Physics.Hurl
 where
 
 
--- NOTE
--- We do not export an equivalent of H.initChipmunk here because it is
--- a no-op. See cpInitChipmunk in chipmunk.c
-
-
 import Physics.Hurl.Space
 import Physics.Hurl.Solid
 import Physics.Hurl.Object
