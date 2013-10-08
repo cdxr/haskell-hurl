54abe298d069b27c677059c591c1030505a01aa7
Craig Roche <cdxr01@gmail.com>
2013-10-08 18:12:44 -0400

    made Solid into a pure ADT; fixed Shape type synonyms

--- Physics/Hurl/Shape.hs
+++ Physics/Hurl/Shape.hs
@@ -1,37 +1,34 @@
-module Physics.Hurl.Shape
-(
-  H.ShapeType
-, Thickness
-, lineSegment
-, circle
-, polygon
-, rectangle
-)
-where
+module Physics.Hurl.Shape (
+    Shape,
+    Thickness,
+    lineSegment,
+    circle,
+    polygon,
+    rectangle,
+    ) where
 
 import Linear.V2
-import Linear.Affine
 
 import qualified Physics.Hipmunk as H
 
 import Physics.Hurl.Internal.Utils
 
 
+type Shape = H.ShapeType
+
 type Thickness = Double
 
-lineSegment :: Thickness -> V2 Double -> V2 Double -> H.ShapeType
+lineSegment :: Thickness -> V2 Double -> V2 Double -> Shape
 lineSegment t a b = H.LineSegment (vectorFromV2 a) (vectorFromV2 b) t
 
-circle :: Double -> H.ShapeType
+circle :: Double -> Shape
 circle = H.Circle
 
-polygon :: [V2 Double] -> H.ShapeType
+polygon :: [V2 Double] -> Shape
 polygon = H.Polygon . map vectorFromV2
 
-rectangle :: Double -> Double -> H.ShapeType
+rectangle :: Double -> Double -> Shape
 rectangle w h = H.Polygon $ map (uncurry H.Vector)
                     [(-x, -y), (-x, y), (x, y), (x, -y)]
       where x = w/2
             y = h/2
-
-
--- Physics/Hurl/Solid.hs
+++ Physics/Hurl/Solid.hs
@@ -1,81 +1,58 @@
-{-# LANGUAGE TemplateHaskell #-}
-
 module Physics.Hurl.Solid
 (
-   Solid
- , solid
- , solidWith
- , Shape
- , shape
- , addToBody
- , momentForSolid
--- * Mutable Properties
- , elasticity
- , friction
-)
-where
+    -- * Solid
+    Solid (..),
+    solid,
+    momentForSolid,
+    -- * Mutable Properties
+    elasticityRef,
+    frictionRef,
+    ) where
+
 
 import Linear.V2
-import Control.Lens.TH
 import Data.StateVar as StateVar
 
 import qualified Physics.Hipmunk as H
+
+import Physics.Hurl.Shape ( Shape )
 
 import Physics.Hurl.Internal.ObjectRef
 
 
 -- TODO
---  move addToBody to an internal module
---  move elasticity and friction to a SolidRef module
---  create a Material type
+--  move elasticityRef and frictionRef to a SolidRef module
+--  ? create a `Material` type
 
-
-type Shape = H.ShapeType
-
-
--- | An entity capable of collisions, attached to a `Body` and consisting of
--- geometry and material properties.
+-- | A collision-capable entity consisting of geometry and material
+-- properties.
 data Solid = Solid
-    { initSolid :: SolidRef -> IO ()
-    , _shape    :: Shape
-    }
+    { elasticity :: !Double
+    , friction   :: !Double
+    , shape      :: !Shape
+    } deriving (Show, Eq, Ord)
 
-$(makeLenses ''Solid)
 
-
--- | Create a `Solid` with default properties from a `Shape`.
+-- | @solid s@ is the `Solid` made of the shape @s@ with zero elasticity
+-- and friction.
 solid :: Shape -> Solid
-solid = Solid (\_ -> return ())
+solid = Solid 0 0
 
 
--- | @momentForSolid s m o@ calculate the moment of intertia for the solid 
+-- | @momentForSolid s m o@ calculates the moment of intertia for the solid 
 -- @s@ of mass @m@ at offset @o@.
 momentForSolid :: Solid -> Double -> V2 Double -> Double
-momentForSolid s m (V2 x y) = H.momentForShape m (_shape s) (H.Vector x y)
+momentForSolid s m (V2 x y) = H.momentForShape m (shape s) (H.Vector x y)
 
-
--- | Create a `Solid` from a shape and a `SolidRef` action.
-solidWith :: Shape -> (SolidRef -> IO ()) -> Solid
-solidWith = flip Solid
-
-
-
--- | @addToBody p s b@ adds the `Solid` @s@ to the Hipmunk `H.Body` at
--- position @p@.
-addToBody :: V2 Double -> Solid -> H.Body -> IO SolidRef
-addToBody (V2 x y) (Solid initSolid shape) b = do
-    ref <- H.newShape b shape (H.Vector x y)
-    initSolid ref
-    return ref
 
 
 -- | The elasticity of the solid in the range 0 to 1.
-elasticity :: SolidRef -> StateVar Double
-elasticity = H.elasticity
+elasticityRef :: SolidRef -> StateVar Double
+elasticityRef = H.elasticity
 
 
 -- | The friction coefficient of the shape according to the Coulumb
 -- friction model. The amount of applied friction is the product of the
 -- friction of both solids.
-friction :: SolidRef -> StateVar Double
-friction = H.friction
+frictionRef :: SolidRef -> StateVar Double
+frictionRef = H.friction
