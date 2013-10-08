eed13a736cd24a07d5a4275b5dd1460af59923ab
Craig Roche <cdxr01@gmail.com>
2013-10-08 17:09:34 -0400

    fixed export list formatting of Physics.Hurl

--- Physics/Hurl.hs
+++ Physics/Hurl.hs
@@ -1,13 +1,11 @@
-module Physics.Hurl
-(
-  module Physics.Hurl.Space
-, module Physics.Hurl.Solid
-, module Physics.Hurl.Object
-, module Physics.Hurl.ObjectRef
-, module Physics.Hurl.Constraint
-, module Physics.Hurl.Query
-)
-where
+module Physics.Hurl (
+    module Physics.Hurl.Space,
+    module Physics.Hurl.Solid,
+    module Physics.Hurl.Object,
+    module Physics.Hurl.ObjectRef,
+    module Physics.Hurl.Constraint,
+    module Physics.Hurl.Query,
+    ) where
 
 
 import Physics.Hurl.Space
