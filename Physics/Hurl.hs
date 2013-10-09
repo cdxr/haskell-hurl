972b7e57cbf55302570d9e01bfe3ddf57dad8ffb
Craig Roche <cdxr01@gmail.com>
2013-10-09 16:55:32 -0400

    added Physics.Hurl.Position to Physics.Hurl export list

--- Physics/Hurl.hs
+++ Physics/Hurl.hs
@@ -1,16 +1,18 @@
 module Physics.Hurl (
-    module Physics.Hurl.Space,
-    module Physics.Hurl.Solid,
     module Physics.Hurl.Object,
+    module Physics.Hurl.Position,
+    module Physics.Hurl.Solid,
+    module Physics.Hurl.Space,
     module Physics.Hurl.ObjectRef,
     module Physics.Hurl.Constraint,
     module Physics.Hurl.Query,
     ) where
 
 
-import Physics.Hurl.Space
-import Physics.Hurl.Solid
 import Physics.Hurl.Object
+import Physics.Hurl.Position
+import Physics.Hurl.Solid
+import Physics.Hurl.Space
 import Physics.Hurl.ObjectRef
 import Physics.Hurl.Constraint
 import Physics.Hurl.Query
