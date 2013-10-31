348a69fc9c2723bd55530f56b2bc255f02c49bbf
Craig Roche <cdxr01@gmail.com>
2013-10-31 15:17:58 -0400

    refactored unit tests

--- test/hunit.hs
+++ test/hunit.hs
@@ -14,6 +14,7 @@ import Test.Tasty.HUnit
 import Test.Tasty.TH
 
 import Physics.Hurl
+import Physics.Hurl.Internal.Space
 
 
 main = $defaultMainGenerator
@@ -23,20 +24,27 @@ basicSolids :: [Solid]
 basicSolids = map solid
     [ Circle 1 0
     , Segment 1 0 10
+    , rectangle 800 50
     , Poly [0, V2 0 10, 10, V2 10 0]
+    , Poly $ reverse [0, V2 0 10, 10, V2 10 0]
     ]
 
 assertSpace :: (Space -> IO a) -> Assertion
 assertSpace f = do
     s <- newSpace
-    f s
+    _ <- f s
     deleteSpace s
 
 case_space :: Assertion
 case_space = assertSpace return
 
-case_single_dynamic :: Assertion
-case_single_dynamic = assertSpace $ addDynamic 0 basicSolids
+case_addObject :: Assertion
+case_addObject = assertSpace $ \s -> do
+    addObject Dynamic 0 basicSolids s
+    addObject Static 0 basicSolids s
 
-case_single_static :: Assertion
-case_single_static = assertSpace $ addStatic 0 basicSolids
+case_addDynamic :: Assertion
+case_addDynamic = assertSpace $ addDynamic 0 basicSolids
+
+case_addStatic :: Assertion
+case_addStatic = assertSpace $ addStatic 0 basicSolids
