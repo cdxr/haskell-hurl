5c2c816fbf78de659285cbc873ef7b6f08699718
Craig Roche <cdxr01@gmail.com>
2013-10-31 15:39:10 -0400

    removed extraneous comments

--- Physics/Hurl.hs
+++ Physics/Hurl.hs
@@ -1,5 +1,4 @@
 module Physics.Hurl (
-    --module Physics.Hurl.Object,
     module Physics.Hurl.Geometry,
     module Physics.Hurl.Solid,
     module Physics.Hurl.Space,
@@ -9,7 +8,6 @@ module Physics.Hurl (
     ) where
 
 
---import Physics.Hurl.Object
 import Physics.Hurl.Geometry
 import Physics.Hurl.Solid
 import Physics.Hurl.Space
